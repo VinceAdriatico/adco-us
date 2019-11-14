0476fac1a59379e18c04a710c56fcbabbb879f0a
Vince Adriatico <vince@adco.us>
2019-11-13 20:25:05 -0800

    11.13

--- adco/inc/template-parts/adco-about-section.php
+++ adco/inc/template-parts/adco-about-section.php
@@ -20,7 +20,7 @@
   // Parts
   $img = image_webp( $option['webp'], $option['bg'] );
 ?>
-<section class="home-section about contain" style="background-image: linear-gradient(to bottom, rgba(0,0,0, 1) 0%, rgba(0,0,0, 0.7) 40%, rgba(0,0,0, 0.1) 100%), url(<?php echo $img; ?>)">
+<section class="home-section about contain" style="background-image: url(<?php echo $img; ?>)">
   <h1 class="title"><?php echo $option['title']; ?></h1>
   <?php echo $option['content']; ?>
 </section>
