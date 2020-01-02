f56f50080568534f57a7c0079e606a5318718dfa
Vince Adriatico <vince@adco.us>
2020-01-01 22:34:30 -0800

    Velocity Adjustments 1.1

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,8 +1,25 @@
-jQuery(document).ready(function() {
+(function($) {
+  jQuery(document).ready(function() {
+    home.init();
 
-  /**
-   * Velocity Animations
-   */
+    function openService(evt, cityName) {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+      document.getElementById(cityName).style.display = "block";
+      evt.currentTarget.className += " active";
+    }
+  });
+
+/**
+ * Velocity Animations
+ */
   var fadeDown = {
     start: {
       translateY: -20,
@@ -61,112 +78,82 @@ jQuery(document).ready(function() {
     }
   };
 
-
-  /**
-   * Home Animations
-   */
-  function titlesFade() {
-    jQuery('.home-section.hero .fadeTitle').delay(3000)
-     .velocity(fadeDown.start, fadeDown.o.slow)
-     .velocity(fadeDown.end, fadeDown.o.fast);
-    jQuery('.home-section.hero .fadeSubtitle').delay(3000)
-     .velocity(fadeUp.start, fadeUp.o.slow)
-     .velocity(fadeUp.end, fadeUp.o.fast);
-  }
-  function heroRows() {
-    // Rows
-    jQuery('.hero-row.delay-3').delay(1300)
-    .velocity(fadeRight.start, fadeRight.o.slow)
-    .velocity(fadeRight.end, fadeRight.o.fast);
-    jQuery('.hero-row.delay-6').delay(1600)
-    .velocity(fadeRight.start, fadeRight.o.slow)
-    .velocity(fadeRight.end, fadeRight.o.fast);
-    jQuery('.hero-row.delay-9').delay(1900)
-    .velocity(fadeRight.start, fadeRight.o.slow)
-    .velocity(fadeRight.end, fadeRight.o.fast);
-    jQuery('.hero-row.delay-12').delay(2200)
-    .velocity(fadeRight.start, fadeRight.o.slow)
-    .velocity(fadeRight.end, fadeRight.o.fast);
-  }
-  function heroBtns() {
-    jQuery('.hero-btn.delay-3').delay(1300)
-    .velocity(fadeDown.start, fadeDown.o.slow)
-    .velocity(fadeDown.end, fadeDown.o.fast);
-    jQuery('.hero-btn.delay-6').delay(1600)
-    .velocity(fadeDown.start, fadeDown.o.slow)
-    .velocity(fadeDown.end, fadeDown.o.fast);
-    jQuery('.hero-btn.delay-9').delay(1900)
-    .velocity(fadeDown.start, fadeDown.o.slow)
-    .velocity(fadeDown.end, fadeDown.o.fast);
-  }
-  function heroFolders() {
-    jQuery('.hero-horizontal').delay(300)
-    .velocity(fadeRight.start, fadeRight.o.slow)
-    .velocity(fadeRight.end, fadeRight.o.fast);
-    jQuery('.hero-vertical').delay(600)
-    .velocity(fadeRight.start, fadeRight.o.slow)
-    .velocity(fadeRight.end, fadeRight.o.fast);
-  }
-  function heroTags() {
-    jQuery('.hero-tag.back').delay(300)
-    .velocity(fadeUp.start, fadeUp.o.slow)
-    .velocity(fadeUp.end, fadeUp.o.fast);
-    jQuery('.hero-tag.front').delay(300)
-    .velocity(fadeRight.start, fadeRight.o.slow)
-    .velocity(fadeRight.end, fadeRight.o.fast);
-    jQuery('.hero-tag.icon-top').delay(600)
-    .velocity(fadeDown.start, fadeDown.o.slow)
-    .velocity(fadeDown.end, fadeDown.o.fast);
-    jQuery('.hero-tag.icon-btm').delay(600)
-    .velocity(fadeUp.start,fadeUp.o.slow)
-    .velocity(fadeUp.end, fadeUp.o.fast);
-  }
-  function browserGen() {
-    jQuery('#hero-icon .background')
-    .velocity(fadeUp.start, fadeUp.o.slow)
-    .velocity(fadeUp.end, fadeUp.o.fast);
-    // ContainerswDdAw
-    jQuery('#hero-icon .menu')
-    .velocity(fadeDown.start, fadeDown.o.slow)
-    .velocity(fadeDown.end, fadeDown.o.fast);
-    jQuery('#hero-icon .bar')
-    .velocity(fadeUp.start, fadeUp.o.slow)
-    .velocity(fadeUp.end, fadeUp.o.fast);
-    jQuery('#hero-icon .body')
-    .velocity(fadeUp.start, fadeUp.o.slow)
-    .velocity(fadeUp.end, fadeUp.o.fast);
-    jQuery('#hero-icon .start')
-    .velocity(fadeDown.start, fadeDown.o.slow)
-    .velocity(fadeDown.end, fadeDown.o.fast);
-
-    heroRows();
-    heroBtns();
-    heroFolders();
-    heroTags();
-  }
-  function homeSequence() {
-
-    // Hero Banner Titles
-    titlesFade();
-
-    // Animate Browser
-    browserGen();
-  }
-
-  // Start Home Sequence
-  homeSequence();
-  
-  function openService(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+/**
+ * Initialize Home Page
+ */
+  var home = {
+    init: function() {
+      this.browser();
+      setTimeout(function() {
+        home.titleFade();
+      }, 3000);
+    },
+    browser: function() {
+      this.browserGen();
+      this.heroFolders();
+      this.heroTags();
+      setTimeout(function() {
+        home.heroRows();
+        home.heroBtns();
+      }, 1000);  
+    },
+    fadeDown: function(selector) {
+      jQuery(selector).velocity(fadeDown.start, fadeDown.o.slow)
+      .velocity(fadeDown.end, fadeDown.o.fast);
+    },
+    fadeUp: function(selector) {
+      jQuery(selector).velocity(fadeUp.start, fadeUp.o.slow)
+      .velocity(fadeUp.end, fadeUp.o.fast);
+    },
+    fadeRight: function(selector) {
+      jQuery(selector).velocity(fadeRight.start, fadeRight.o.slow)
+      .velocity(fadeRight.end, fadeRight.o.fast);
+    },
+    titleFade: function() {
+      home.fadeDown('.home-section.hero .fadeTitle');
+      home.fadeUp('.home-section.hero .fadeSubtitle');
+    },
+    heroRows: function() {
+      home.fadeRight('.hero-row.delay-3');
+      setTimeout(function() {
+        home.fadeRight('.hero-row.delay-6');
+      }, 300);
+      setTimeout(function() {
+        home.fadeRight('.hero-row.delay-9');
+      }, 600);
+      setTimeout(function() {
+        home.fadeRight('.hero-row.delay-12');
+      }, 900);
+    },
+    heroBtns: function() {
+      home.fadeDown('.hero-btn.delay-3')
+      setTimeout(function() {
+        home.fadeDown('.hero-btn.delay-6');
+      }, 300);
+      setTimeout(function() {
+        home.fadeDown('.hero-btn.delay-9');
+      }, 600);
+    },
+    heroFolders: function() {
+      home.fadeRight('.hero-horizontal');
+      setTimeout(function() {
+        home.fadeRight('.hero-vertical');
+      }, 300);
+    },
+    heroTags: function() {
+      home.fadeUp('.hero-tag.back');
+      home.fadeRight('.hero-tag.front');
+      setTimeout(function() {
+        home.fadeDown('.hero-tag.icon-top');
+        home.fadeUp('.hero-tag.icon-btn');
+      }, 300)
+    },
+    browserGen: function() {
+      home.fadeUp('#hero-icon .background');
+      home.fadeDown('#hero-icon .menu');
+      home.fadeUp('#hero-icon .bar');
+      home.fadeUp('#hero-icon .body');
+      home.fadeDown('#hero-icon .start');
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
   }
-});
+})(jQuery)
